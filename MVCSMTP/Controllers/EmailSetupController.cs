c176b442ef20ad4ec034b017c4f7dd48ebf86320
Balaji S <balaji.sandupatla@hotmail.com>
2019-11-04 17:26:50 +0530

    Add project files.

---
+++ MVCSMTP/Controllers/EmailSetupController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCSMTP.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace MVCSMTP.Controllers
+{
+    public class EmailSetupController : Controller
+    {
+        // GET: EmailSetup
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(MVCSMTP.Models.Gmail model)
+        {
+            MailMessage mm = new MailMessage("balaji.sandupatla@gmail.com", model.To)
+            {
+                Subject = model.Subject,
+                Body = model.Body,
+                IsBodyHtml = false
+            };
+
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true
+            };
+
+            NetworkCredential nc = new NetworkCredential("balaji.sandupatla@gmail.com", "balaji@paru");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = nc;
+            smtp.Send(mm);
+            ViewBag.Message = "Mail Has Been Sent Successfully!";
+
+
+            return View();
+        }
+    }
+}
